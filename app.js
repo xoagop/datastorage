70a31f8959eb132768f9746da81ae83f81f32688
Anna Fröblom <anna.froblom@gmail.com>
2015-03-24 18:42:29 +0200

    Separated first and second controllers to their own views and used location service to navigate from first to second.

--- app.js
+++ app.js
@@ -1,4 +1,19 @@
-var app = angular.module('myApp', []);
+var app = angular.module('myApp', ['ngRoute']);
+
+app.config(['$routeProvider', function($routeProvider) {
+  $routeProvider
+    .when('/', {
+      controller: 'firstCtrl',
+      templateUrl: 'first.html'
+    })
+    .when('/second', {
+      controller: 'secondCtrl',
+      templateUrl: 'second.html'
+    })
+    .otherwise({
+      redirectTo: '/'
+    });
+}]);
 
 app.factory('dataStorage', [function () {
   function Storage() {
@@ -16,9 +31,10 @@ app.factory('dataStorage', [function () {
   return new Storage();
 }]);
 
-app.controller('firstCtrl', ['$scope', 'dataStorage', function ($scope, dataStorage) {
+app.controller('firstCtrl', ['$scope', '$location', 'dataStorage', function ($scope, $location, dataStorage) {
   $scope.saveText = function () {
     dataStorage.setData($scope.inputText);
+    $location.path('/second');
   };
 }]);
 
