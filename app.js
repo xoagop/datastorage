b909bcff6d05226375e2ef7bc5f815714a167323
Anna Fröblom <anna.froblom@gmail.com>
2015-03-24 18:05:46 +0200

    Data storage implemented.

---
+++ app.js
@@ -0,0 +1,29 @@
+var app = angular.module('myApp', []);
+
+app.factory('dataStorage', [function() {
+    function Storage() {
+        this.data = 'YES';
+        
+        this.getData = function() {
+            return this.data;
+        };
+        
+        this.setData = function(data) {
+            this.data = data;
+        }
+    };
+    
+    return new Storage();
+}]);
+
+app.controller('firstCtrl', ['$scope', 'dataStorage', function($scope, dataStorage) {
+    $scope.saveText = function(){
+      dataStorage.setData($scope.inputText);  
+    };
+}]);
+
+app.controller('secondCtrl', ['$scope', 'dataStorage', function($scope, dataStorage) {
+    $scope.text = function() {
+        return dataStorage.getData();
+    };
+}]);
