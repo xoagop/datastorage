0ae1aa8fc5bccb7058b66ac456280f0bcfe748a2
Anna Fröblom <anna.froblom@gmail.com>
2015-03-24 18:13:58 +0200

    Beautified code and added angular route.

--- app.js
+++ app.js
@@ -1,29 +1,29 @@
 var app = angular.module('myApp', []);
 
-app.factory('dataStorage', [function() {
-    function Storage() {
-        this.data = 'YES';
-        
-        this.getData = function() {
-            return this.data;
-        };
-        
-        this.setData = function(data) {
-            this.data = data;
-        }
+app.factory('dataStorage', [function () {
+  function Storage() {
+    this.data = 'YES';
+
+    this.getData = function () {
+      return this.data;
     };
-    
-    return new Storage();
+
+    this.setData = function (data) {
+      this.data = data;
+    }
+  };
+
+  return new Storage();
 }]);
 
-app.controller('firstCtrl', ['$scope', 'dataStorage', function($scope, dataStorage) {
-    $scope.saveText = function(){
-      dataStorage.setData($scope.inputText);  
-    };
+app.controller('firstCtrl', ['$scope', 'dataStorage', function ($scope, dataStorage) {
+  $scope.saveText = function () {
+    dataStorage.setData($scope.inputText);
+  };
 }]);
 
-app.controller('secondCtrl', ['$scope', 'dataStorage', function($scope, dataStorage) {
-    $scope.text = function() {
-        return dataStorage.getData();
-    };
+app.controller('secondCtrl', ['$scope', 'dataStorage', function ($scope, dataStorage) {
+  $scope.text = function () {
+    return dataStorage.getData();
+  };
 }]);
